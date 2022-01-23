f921b37e30ff38d37d916386c7bb6ff0850b8670
BrettBass <bbass@ucsd.edu>
2022-01-23 00:34:29 -0800

    finished casino, games to be added

--- gamba/wubbaLubbaDubDubCasino.py
+++ gamba/wubbaLubbaDubDubCasino.py
@@ -239,3 +239,85 @@ class wubba(commands.Cog):
         await self.config.member(ctx.author).blemflarks.set(user_blemflarks)
         await ctx.reply(f"""You have received {interest} {currency} {'s' if interest > 1 else ''} from Gringotts™ Bank. Today's interest rate was {rate}%. 
                         Thank you for trusting Gringotts™ Bank.""")
+
+    @wubba.command(usage="<User> <BlemFlark Amount>")
+    async def setBlemFlarks(self, ctx, user: discord.Member, amount: int):
+        """
+        owner command to manually change blemflark amount
+        """
+        if ctx.guild is None:
+            await ctx.send("Unavailable in DM's")
+            return
+        
+        await self.config.memeber(user).blemflarks.set(amount)
+        await ctx.reply("done.")
+
+    @blemflark.command(usage="<User> <Amount>", aliases=['p', "transfer", "spreadit"], autohelp=False)
+    async def pay(self, ctx, recipient: discord.Member, amount: int):
+        """
+        Pay a user with some of your hard earned blemflarks
+        """
+        try:
+            if self.game_session_active[ctx.author.id]:
+                await ctx.reply("Not during game bitch.")
+                return
+
+            if self.game_session_active[recipient.id]:
+                await ctx.reply("He's in a game bitch.")
+                return
+        
+        except:
+            pass
+        
+        if await self.config.member(ctx.author).banned() or await self.loop.create_task(wubba.invalid_chanel(self, ctx)): return
+
+        if recipient is int:
+            recipient = await self.bot.fetch_user(recipient)
+        
+        user_blemflark_counter = await self.config.member(ctx.author).blemflarks()
+        recipient_blemflark_counter = await self.config.member(recipient).blemflarks()
+
+        if recipient.id == ctx.author.id:
+            await ctx.reply("You can't pay yourself dingus.")
+            return
+        
+        if amount <= 0:
+            await ctx.reply("Amount must be greater than 0.")
+            return
+        
+        if amount > user_blemflark_counter:
+            await ctx.reply(f"You don't have enough {currency}s chief.")
+            return
+        
+        self.game_session_active[ctx.author.id] = True
+
+        warning_msg = await ctx.reply(f"""Are you sure you want to send {amount} {currency}{'s' if amount > 1 else ''} to {recipient.name}?
+                                     {'9% tax will be deducted from this transaction.' if amount > 50 else ''}""")
+
+        pred = ReactionPredicate.yes_or_no(warning_msg, ctx.author)
+        start_adding_reactions(warning_msg, ReactionPredicate.YES_OR_NO_EMOJIS)
+
+        try:
+            await self.bot.wait_for('reaction_add', check=pred, timeout=30)
+        except asyncio.TimeoutError:
+            await warning_msg.edit(content="Transaction timed out.") 
+            await warning_msg.clear_reactions()
+            self.game_session_active[ctx.author.id] = False
+            return
+
+        if not  pred.result:
+            await warning_msg.edit(content="Transaction cancelled.")
+            self.game_session_active[ctx.author.id] = False
+            return
+
+        user_blemflark_counter -= amount
+        tax = 0.09 if amount > 50 else 0 
+        amount -= int(amount * tax)
+        
+        recipient_blemflark_counter += amount
+
+        await self.config.memeber(ctx.author).blemflarks.set(user_blemflark_counter)
+        await self.config.memeber(recipient).blemflarks.set(recipient_blemflark_counter)
+        await ctx.reply(f"paid {recipient.name} {amount} {currency}{'s' if amount > 1 else ''}.")
+        self.game_session_active[ctx.author.id] = False
+
