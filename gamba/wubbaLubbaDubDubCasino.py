e62de83104df8578d2384b88d990baf97f77bbf2
BrettBass <bbass@ucsd.edu>
2022-01-22 17:17:40 -0800

    can now control which channels the bot can be used in and which users can use it

--- gamba/wubbaLubbaDubDubCasino.py
+++ gamba/wubbaLubbaDubDubCasino.py
@@ -15,7 +15,7 @@ from .quackjack import quackjack
 
 currency = "Blemflarck"
 
-class WubbaLubbaDubDub(commands.Cog):
+class wubba(commands.Cog):
     """
     wubba lubba dub dub Casino is a place for pure degeneracy
     """
@@ -30,8 +30,64 @@ class WubbaLubbaDubDub(commands.Cog):
         self.loop = bot.loop
         self.activeGames = {}
     
-    @commands.group(alias=["wubba"])
-    async def wubbalubbadub(self, ctx):
+    @commands.group(alias=["wc"])
+    async def wubba(self, ctx):
         pass
 
+    @wubba.command(usage="<User>", aliases=["ban"])
+    async def blacklist(self, ctx, user:discord.Member):
+        """
+        Blacklist a user from the wubba lubba dub dub Casino
+        """
+        await self.config.member(user).banned.set(True)
+        await ctx.reply(f"{user.name} has been blacklisted from the wubba lubba dub dub Casino")
+    
+    @wubba.command(usage="<User>", aliases=["unban"])
+    async def whitelist(self, ctx, user:discord.Member):
+        """
+        Whitelist a user from the wubba lubba dub dub Casino
+        """
+        await self.config.member(user).banned.set(False)
+        await ctx.reply(f"{user.name} can now enter/gamble at wubba lubba dub dub Casino without being shot")
+    
+    @wubba.group()
+    async def gambaChannels(self, ctx):
+        pass
+
+    @gambaChannels.command(usage="<text channels (space seperated)>")
+    async def add(self, ctx, *channels:discord.TextChannel):
+        """
+        Add gamba channels
+        """
+        gambaChannels = await self.config.guild(ctx.guild).gambaChannels()
+        channels_added = ""
+        
+        for channel in channels:
+            gambaChannels.append(channel.name)
+            channels_added += f"{channel.name} "
+        
+        await self.config.guild(ctx.guild).gambaChannels.set(gambaChannels)
+        await ctx.reply(f"Added {channels_added} to gamba channel list")
+    
+    @gambaChannels.command(usage="<text channels (space seperated)>")
+    async def remove(self, ctx, *channels:discord.TextChannel):
+        """
+        Remove gamba channels
+        """
+        gambaChannels = await self.config.guild(ctx.guild).gambaChannels()
+        channels_removed = ""
+        not_found = ""
+        
+        for channel in channels:
+            try:
+                gambaChannels.remove(channel.name)
+                channels_removed += f"{channel.name} "
+            except:
+                not_found += f"{channel.name} "
+        
+        await self.config.guild(ctx.guild).gambaChannels.set(gambaChannels)
+        await ctx.reply(f"Removed {channels_removed} from gamba channels")
+        
+        if not_found != "":
+            await ctx.send(f"channel{'s' if len(not_found) > 1 else ''} {not_found} not in gamba channel")
     
